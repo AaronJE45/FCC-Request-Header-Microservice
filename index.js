3e7586489f30fa26a5c1e1a50d89d648ef6b44cd
Aaron Joshua Emmanuel <ajemmanuel45@outlook.com>
2024-10-17 14:51:11 +0530

    Project Complete

--- index.js
+++ index.js
@@ -19,12 +19,20 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.get("/api/whoami", (req,res) =>{
+  const ipaddress = req.socket.remoteAddress;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress, language, software
+  })
 });
 
+
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000,'0.0.0.0', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
